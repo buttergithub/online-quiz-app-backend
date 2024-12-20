15462a493ffcf644863902e276bc7a9f1c873a39
Mag codes <muneangechaste@gmail.com>
2024-12-20 22:25:57 +0200

    Added real origin for cors

--- src/main/java/com/auca/onlineQuizApp/controller/WebConfig.java
+++ src/main/java/com/auca/onlineQuizApp/controller/WebConfig.java
@@ -10,11 +10,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("https://online-quiz-new1.netlify.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization")
-                .allowCredentials(false)
+                .allowCredentials(true)
                 .maxAge(3600);
     }
 }
